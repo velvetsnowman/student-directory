1207f38868ceb4962bbb3832c3eea4bdaaa8321c
EC2 Default User <ec2-user@ip-172-31-31-54.eu-west-1.compute.internal>
2018-06-27 08:58:09 +0000

    names and cohort info

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: "November"},
+  {name: "Darth Vader", cohort: "November"},
+  {name: "Nurse Ratched", cohort: "November"},
+  {name: "Michael Corleone", cohort: "November"},
+  {name: "Alex DeLarge", cohort: "November"},
+  {name: "The Wicked Witch of the West", cohort: "November"},
+  {name: "Terminator", cohort: "November"},
+  {name: "Freddy Krueger", cohort: "November"},
+  {name: "The Joker", cohort: "November"},
+  {name: "Joffrey Baratheon", cohort: "November"},
+  {name: "Norman Bates", cohort: "November"}
 ]
 
 def print_header
@@ -17,14 +17,14 @@ def print_header
     puts "----------------------------"
 end
 
-def print(names)
-    names.each do |name|
-        puts name
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students."
+def print_footer(students)
+    puts "Overall, we have #{students.count} great students."
 end
 
 print_header
