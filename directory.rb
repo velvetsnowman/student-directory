ecce4cb0eb5d8256afb4a525c57e0901c66b296f
EC2 Default User <ec2-user@ip-172-31-31-54.eu-west-1.compute.internal>
2018-06-28 16:18:50 +0000

    Step 8: Excercise 4

--- directory.rb
+++ directory.rb
@@ -1,13 +1,16 @@
-
 def print_header
     puts "The students of Villains Academy"
     puts "----------------------------"
 end
 
 def print(students)
-    students.each do |student|
-        if student[:name].length < 12   
-          puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    accumulator = 0
+    while accumulator <= @students.count - 1 do
+        if  !@students[accumulator].empty?
+            puts "#{@students[accumulator][:name]} (#{@students[accumulator][:cohort]} cohort)"
+            accumulator += 1
+        else
+            break
         end
     end
 end
@@ -19,15 +22,17 @@ end
 def input_students
     puts "Please enter the names of the students you want to add: "
     puts "(To finish, just hit return twice)"
-    students = []
+    @students = []
     name = gets.chomp
     while !name.empty? do
-        students << {name: name, cohort: :November}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :November}
+        puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
-    students
+    @students
 end    
 
-students = input_students
-print(students)
+@students = input_students
+print_header
+print(@students)
+print_footer(@students)
