edab77dc9f5109913a5684c868e1b74797cb4321
EC2 Default User <ec2-user@ip-172-31-31-54.eu-west-1.compute.internal>
2018-06-29 13:04:34 +0000

    Step 8: Excercise 7

--- directory.rb
+++ directory.rb
@@ -1,32 +1,37 @@
-@linewidth = 40
-
 def print_header
-    puts "The Students of Villains Academy".center(@linewidth)
-    puts "-----------------------------------------".center(@linewidth)
+    puts "The students of Villains Academy"
+    puts "------------------------------------"
 end
 
 def print(arguement)
     arguement.each do |x|
-        puts ("#{x[:name]} (#{x[:cohort]} cohort)").center(@linewidth)
+        puts "#{x[:name]} (#{x[:cohort]} cohort)"
     end
 end
 
 def print_footer(arguement)
-    puts "Overall, we have #{arguement.count} great students.".center(@linewidth)
+    puts "Overall, we have #{arguement.count} great students."
 end
 
 def input_students
     puts "Please enter the names of the students you want to add: "
     puts "(To finish, just hit return twice)"
     @students = []
-    name = gets.chomp
+    name = gets.chomp.capitalize
     while !name.empty? do
-        @students << {name: name, cohort: :November}
-        puts "Now we have #{@students.count} students"
-        name = gets.chomp
+        puts "What cohort are they in?"
+        cohort = gets.chomp.capitalize
+        if cohort == "January" || cohort == "February" || cohort == "March" || cohort == "April" || cohort == "May" || cohort == "June" || cohort == "July" || cohort == "August" || cohort == "September" || cohort == "October" || cohort == "November" || cohort == "December"
+          @students << {name: name, cohort: cohort.to_sym}
+          puts "Now we have #{@students.count} students"
+          puts "Next student:"
+          name = gets.chomp.capitalize
+        else
+          puts "That month doesn't exist, check your spelling!"
+        end
     end
     @students
-end    
+end
 
 @students = input_students
 print_header
