b8579084ad1cd303bbfef3b83269148ea6200a73
EC2 Default User <ec2-user@ip-172-31-31-54.eu-west-1.compute.internal>
2018-06-30 09:38:03 +0000

    Step 10: Refactored interactive menu

--- directory.rb
+++ directory.rb
@@ -1,34 +1,61 @@
-def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+@students = []
+
+def interactive_menu
+    loop do
+      print_menu
+      process(gets.chomp)
+    end
 end
 
-def print(arguement)
-  arguement.each do |x|
-    puts "#{x[:name]} (#{x[:cohort]} cohort)"
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" 
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
   end
-end
-
-def print_footer(arguement)
-  puts "Overall, we have #{arguement.count} great students"
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  @students = []
-  name = gets.chomp
-  while !name.empty? do
-    @students << {name: name, cohort: :November}
-    puts "Now we have #{@students.count} students"
+    puts "Please enter the names of the students you want to add: "
+    puts "(To finish, just hit return twice)"
     name = gets.chomp
-  end
-  @students
+    while !name.empty? do
+        @students << {name: name, cohort: :November}
+        puts "Now we have #{@students.count} students"
+        name = gets.chomp
+    end
+end  
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
-@students = input_students
-print_header
-print(@students)
-print_footer(@students)
+def print_header
+    puts "The students of Villains Academy"
+    puts "--------------------------------"
+end
 
-#ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+def print_students_list
+    @students.each do |x|
+        puts "#{x[:name]} (#{x[:cohort]} cohort)"
+    end
+end
+
+def print_footer
+    puts "Overall, we have #{@students.count} great students."
+end 
+
+interactive_menu
