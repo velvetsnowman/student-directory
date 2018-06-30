60e01a9ba99841a4536f1dcc93037e2f3018832d
EC2 Default User <ec2-user@ip-172-31-31-54.eu-west-1.compute.internal>
2018-06-30 08:48:22 +0000

    Step 8: Excercise 11

--- directory.rb
+++ directory.rb
@@ -1,52 +1,34 @@
 def print_header
-    puts "The Students of Villains Academy"
-    puts "--------------------------------"
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
 def print(arguement)
-    arguement.each do |x|
-        puts "#{x[:name]} (#{x[:cohort]} cohort)"
-    end
+  arguement.each do |x|
+    puts "#{x[:name]} (#{x[:cohort]} cohort)"
+  end
 end
 
 def print_footer(arguement)
-    if arguement.count <= 1
-      puts "Overall, we have #{arguement.count} great student."
-    else
-      puts "Overall, we have #{arguement.count} great students."
-    end
+  puts "Overall, we have #{arguement.count} great students"
 end
 
 def input_students
-    puts "Please enter the names of the students you want to add: "
-    puts "(To finish, just hit return twice)"
-    @students = []
-    name = gets.chomp.capitalize
-    while !name.empty? do
-        puts "What cohort are they in?"
-        cohort = gets.chomp.capitalize
-        if cohort == "January" || cohort == "February" || cohort == "March" || cohort == "April" || cohort == "May" || cohort == "June" || cohort == "July" || cohort == "August" || cohort == "September" || cohort == "October" || cohort == "November" || cohort == "December"
-          @students << {name: name, cohort: cohort.to_sym}
-          if @students.count <= 1
-            puts "Now we have #{@students.count} student"
-            puts "Next student:"
-          else 
-            puts "Now we have #{@students.count} students"
-            puts "Next student:"
-          end
-          name = gets.chomp.capitalize
-        else
-          puts "That month doesn't exist, check your spelling!"
-        end
-    end
-    @students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  @students = []
+  name = gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :November}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+  @students
 end
 
 @students = input_students
-if @students.count > 0
-  print_header
-  print(@students)
-  print_footer(@students)
-else
-  puts "There are no students :("
-end
+print_header
+print(@students)
+print_footer(@students)
+
+#ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
