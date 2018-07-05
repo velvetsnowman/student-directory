c8a616d8bf1f1ba0f17067206290b6b0d7ba8735
velvetsnowman <daniel.mista@gmail.com>
2018-07-05 18:57:42 +0100

    Step 8. Exercise 8

--- directory.rb
+++ directory.rb
@@ -1,99 +1,44 @@
-@students = []
-
-def interactive_menu
-    loop do
-      print_menu
-      process(STDIN.gets.chomp)
-    end
+def print_header
+    puts "The students of Villains Academy"
+    puts "------------------------------------"
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save students to students.csv"
-  puts "4. Load students from students.csv"
-  puts "9. Exit"
+def print(arguement)
+    puts "What cohort do you want to search for?"
+    inputed_cohort = gets.chomp.to_sym
+    @students.map {|item|
+      if item[:cohort] == inputed_cohort
+        puts "#{item[:name]} (#{item[:cohort]} cohort)"
+      end
+    }
+
 end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-  end
-end
-
-def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |x|
-    student_data = [x[:name], x[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def try_load_students
-  filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
-    load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else
-    puts "Sorry, #{filename} doesn't exist."
-    exit
-  end
+def print_footer(arguement)
+    puts "Overall, we have #{arguement.count} great students."
 end
 
 def input_students
     puts "Please enter the names of the students you want to add: "
     puts "(To finish, just hit return twice)"
-    name = STDIN.gets.chomp
+    @students = []
+    name = gets.chomp.capitalize
     while !name.empty? do
-        @students << {name: name, cohort: :November}
-        puts "Now we have #{@students.count} students"
-        name = STDIN.gets.chomp
+        puts "What cohort are they in?"
+        cohort = gets.chomp.capitalize
+        if cohort == "January" || cohort == "February" || cohort == "March" || cohort == "April" || cohort == "May" || cohort == "June" || cohort == "July" || cohort == "August" || cohort == "September" || cohort == "October" || cohort == "November" || cohort == "December"
+          @students << {name: name, cohort: cohort.to_sym}
+          puts "Now we have #{@students.count} students"
+          puts "Next student:"
+          name = gets.chomp.capitalize
+        else
+          puts "That month doesn't exist, check your spelling!"
+        end
     end
+    @students
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def print_header
-    puts "The students of Villains Academy"
-    puts "--------------------------------"
-end
-
-def print_students_list
-    @students.each do |x|
-        puts "#{x[:name]} (#{x[:cohort]} cohort)"
-    end
-end
-
-def print_footer
-    puts "Overall, we have #{@students.count} great students."
-end
-
-try_load_students
-interactive_menu
+@students = input_students
+print_header
+print(@students)
+print_footer(@students)
