41cfd088828d0dea15892951499151e62558c27e
velvetsnowman <daniel.mista@gmail.com>
2018-07-06 16:35:54 +0100

    all exercises together

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
     loop do
       print_menu
-      process(STDIN.gets.chomp)
+      process(gets.chomp)
     end
 end
 
@@ -35,7 +35,7 @@ end
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |x|
-    student_data = [x[:name], x[:cohort]]
+    student_data = [x[:name], x[:cohort], x[:age], x[:height], x[:colour]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
@@ -57,31 +57,53 @@ end
 def input_students
     puts "Please enter the names of the students you want to add: "
     puts "(To finish, just hit return twice)"
-    @name = STDIN.gets.chomp
+    @name = gets.chomp
     while !@name.empty? do
-        add_to_array
-        puts "Now we have #{@students.count} students"
-        @name = STDIN.gets.chomp
+        puts "What cohort are they in?"
+        @cohort = gets.chomp.capitalize
+        if @cohort == "January" || @cohort == "February" || @cohort == "March" || @cohort == "April" || @cohort == "May" || @cohort == "June" || @cohort == "July" || @cohort == "August" || @cohort == "September" || @cohort == "October" || @cohort == "November" || @cohort == "December"
+          puts "And his or her age?"
+          @age = gets.chomp
+          puts "And their height?"
+          @height = gets.chomp.to_s
+          puts "And their favourite colour?"
+          @colour = gets.chomp
+          add_to_array
+          if @students.count == 1
+            puts "Now we have #{@students.count} student"
+            puts "Next student:"
+          else
+            puts "Now we have #{@students.count} students"
+            puts "Next student:"
+          end
+          @name = gets.chomp
+        else
+          puts "That month doesn't exist, check your spelling!"
+        end
     end
 end
 
 def add_to_array
-  @students << {name: @name, cohort: :November}
+  @students << {name: @name, cohort: @cohort.to_sym, age: @age, height: @height, colour: @colour}
 end
 
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
-    @name, cohort = line.chomp.split(',')
+    @name, @cohort, @age, @height, @colour = line.chomp.split(',')
     add_to_array
   end
   file.close
 end
 
 def show_students
-  print_header
-  print_students_list
-  print_footer
+  if @students.count > 0
+    print_header
+    print_students_list
+    print_footer
+  else
+    puts "There are no students to print :("
+  end
 end
 
 def print_header
@@ -90,13 +112,19 @@ def print_header
 end
 
 def print_students_list
+    acc = 1
     @students.each do |x|
-        puts "#{x[:name]} (#{x[:cohort]} cohort)"
+        puts "#{acc}. #{x[:name]} (#{x[:cohort]} cohort) - Age: #{x[:age]} | Height: #{x[:height]} | Favourite Colour: #{x[:colour]}"
+        acc += 1
     end
 end
 
 def print_footer
-    puts "Overall, we have #{@students.count} great students."
+    if @students.count == 1
+      puts "Overall, we have #{@students.count} great student."
+    else
+      puts "Overall, we have #{@students.count} great students."
+    end
 end
 
 try_load_students
